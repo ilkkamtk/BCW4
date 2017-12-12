88fc339499ca4cef42c2ad2faeeddde7fe8f3eef
ilkkamtk <ilkka.kylmaniemi@metropolia.fi>
2017-12-12 16:53:18 +0200

    update

--- js/main-b.js
+++ js/main-b.js
@@ -33,7 +33,38 @@ const showImages = () => {
         </li>`;
     });
     ul.innerHTML = html;
+    // now links can be selected
+    linkActions();
   });
 };
 
 showImages();
+
+const linkActions = () => {
+  // select all a elements inside ul
+  const links = document.querySelectorAll('ul a');
+  // select img elment inside modal
+  const img = document.querySelector('#modal img');
+  // loop links
+  links.forEach((link) => {
+    // when link is clicked
+    link.addEventListener('click', (evt) => {
+      // prevent default action
+      evt.preventDefault();
+      // get href value of link
+      const href = link.href;
+      // set modal image src to href value
+      img.src = href;
+      // display modal (use lightbox class)
+      document.querySelector('#modal').classList.replace('hidden', 'lightbox');
+    });
+  });
+};
+
+// select close button of modal
+const closeButton = document.querySelector('.closeBtn');
+// when close button is clicked hide modal
+closeButton.addEventListener('click', (evt) => {
+  evt.preventDefault();
+  document.querySelector('#modal').classList.replace('lightbox', 'hidden');
+});
